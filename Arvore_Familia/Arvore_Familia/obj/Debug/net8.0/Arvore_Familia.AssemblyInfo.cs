c6c38ad5664372df6a5459e2bbedfaabc390fa3c
Osvaldo Dias Junior <odias5288@gmail.com>
2024-11-12 20:13:22 -0300

    Alteração do mwu codigo

--- Arvore_Familia/Arvore_Familia/obj/Debug/net8.0/Arvore_Familia.AssemblyInfo.cs
+++ Arvore_Familia/Arvore_Familia/obj/Debug/net8.0/Arvore_Familia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arvore_Familia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab236a138c2ccb06ad02c7bc5fe5231d69bd8165")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c14117506aeb2f39088fa922217a9cfcd39c5c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arvore_Familia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arvore_Familia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
